fc33431218d463a95244dc3305338ee2f8890133
Andy Guinder <andreabelen.guinder@gmail.com>
2026-07-24 18:13:23 -0300

    mejoro mi commit anterior y sumo cosas: elimino modal login para crear pagina login y creo checkout que es una ruta protegida, sumo estilos

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '../styles/Pages.module.scss';
 
 function Checkout() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h2>¡Proceso de Compra!</h2>
+    <div className={styles.checkout}>
+      <h2>¡Llegaste al Checkout!</h2>
       <p>Esta es una vista protegida. Solo podés ver esto si estás logueado/a.</p>
     </div>
   );
